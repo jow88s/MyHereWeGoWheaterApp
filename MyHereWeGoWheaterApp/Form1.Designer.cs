a99cf0bee35e8371ba933e274c0571480f0b7def
Jow 88s <jow88s@gmail.com>
2020-03-29 14:03:04 +0200

    Add project files.

---
+++ MyHereWeGoWheaterApp/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MyHereWeGoWheaterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._flp = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnHechtel = new System.Windows.Forms.Button();
+            this.btnAntwerpen = new System.Windows.Forms.Button();
+            this.btnOostende = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _flp
+            // 
+            this._flp.AutoSize = true;
+            this._flp.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowOnly;
+            this._flp.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this._flp.Location = new System.Drawing.Point(12, 80);
+            this._flp.Name = "_flp";
+            this._flp.Size = new System.Drawing.Size(0, 0);
+            this._flp.TabIndex = 0;
+            this._flp.WrapContents = false;
+            // 
+            // btnHechtel
+            // 
+            this.btnHechtel.Location = new System.Drawing.Point(12, 12);
+            this.btnHechtel.Name = "btnHechtel";
+            this.btnHechtel.Size = new System.Drawing.Size(90, 50);
+            this.btnHechtel.TabIndex = 1;
+            this.btnHechtel.Text = "Hechtel";
+            this.btnHechtel.UseVisualStyleBackColor = true;
+            this.btnHechtel.Click += new System.EventHandler(this.btnHechtel_Click);
+            // 
+            // btnAntwerpen
+            // 
+            this.btnAntwerpen.Location = new System.Drawing.Point(146, 12);
+            this.btnAntwerpen.Name = "btnAntwerpen";
+            this.btnAntwerpen.Size = new System.Drawing.Size(90, 50);
+            this.btnAntwerpen.TabIndex = 2;
+            this.btnAntwerpen.Text = "Antwerpen";
+            this.btnAntwerpen.UseVisualStyleBackColor = true;
+            this.btnAntwerpen.Click += new System.EventHandler(this.btnAntwerpen_Click);
+            // 
+            // btnOostende
+            // 
+            this.btnOostende.Location = new System.Drawing.Point(289, 12);
+            this.btnOostende.Name = "btnOostende";
+            this.btnOostende.Size = new System.Drawing.Size(90, 50);
+            this.btnOostende.TabIndex = 3;
+            this.btnOostende.Text = "Oostende";
+            this.btnOostende.UseVisualStyleBackColor = true;
+            this.btnOostende.Click += new System.EventHandler(this.btnOostende_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 450);
+            this.Controls.Add(this.btnOostende);
+            this.Controls.Add(this.btnAntwerpen);
+            this.Controls.Add(this.btnHechtel);
+            this.Controls.Add(this._flp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel _flp;
+        private System.Windows.Forms.Button btnHechtel;
+        private System.Windows.Forms.Button btnAntwerpen;
+        private System.Windows.Forms.Button btnOostende;
+    }
+}
+
---
+++ MyHereWeGoWheaterApp/Form1.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHereWeGoWheaterApp
+{
+    public partial class Form1 : Form
+    {
+        private Size _flpSize = new Size(800, 600);
+        private HttpClient _httpClient = new HttpClient();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // ///////////////
+
+        private void ClearDynamicInsertedControls()
+        {
+            this.Controls.Remove((Control)this._flp);
+        }
+
+        private void ShowWeatherForecast_7days(string location)
+        {
+            this.ClearDynamicInsertedControls();
+            int x = 20;
+            int y = 90;
+            int num = 80;
+            this.Text = "Weersvoorspelling voor 7 dagen";
+            try
+            {
+                HttpResponseMessage result1 = this._httpClient.GetAsync("https://weather.api.here.com/weather/1.0/report.json?app_id=VjXo59SzFVv1SseP8Dxi&app_code=WUutcvuv5eTkLeaVyrd5uQ&language=nl-NL&product=forecast_7days&name=" + location).GetAwaiter().GetResult();                
+                if (result1.StatusCode.ToString() == "OK")
+                {
+                    string result2 = result1.Content.ReadAsStringAsync().Result;
+
+                    JArray jarray = (JArray)((JObject)((JObject)JObject.Parse(result2).GetValue("forecasts")).GetValue("forecastLocation")).GetValue("forecast");
+                    this._flp = new FlowLayoutPanel();
+                    this._flp.Location = new Point(x, y);
+                    this._flp.Size = this._flpSize;
+                    this._flp.AutoScroll = true;
+                    foreach (JObject jobject in jarray)
+                    {
+                        DateTime dateTime = DateTime.Parse(jobject.GetValue("utcTime").ToString(), (IFormatProvider)null);
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Name = "mypicturebox";
+                        pictureBox.Load(jobject.GetValue("iconLink").ToString());
+                        pictureBox.Location = new Point(x, y);
+                        this._flp.Controls.Add((Control)pictureBox);
+                        Label label = new Label();
+                        label.Name = "mylabel";
+                        label.Location = new Point(x + 100, y + 5);
+                        label.Size = new Size(600, 90);
+                        label.Text = jobject.GetValue("weekday").ToString() + " (" + dateTime.ToString("dd-MMM-yyyy") + ") - " + jobject.GetValue("daySegment").ToString() + Environment.NewLine + "    " + jobject.GetValue("description")?.ToString() + " " + jobject.GetValue("beaufortDescription")?.ToString() + "   [ " + jobject.GetValue("temperature")?.ToString() + "°C ]" + Environment.NewLine + "    Regenkans: " + jobject.GetValue("precipitationProbability")?.ToString() + "% - GevoelsTemp: " + jobject.GetValue("comfort").ToString() + " °C";
+                        this._flp.Controls.Add((Control)label);
+                        y += num;
+                    }
+                    this.Controls.Add((Control)this._flp);
+                }
+                else
+                    this.ShowWeatherForecast_7days_simple(location);
+            }
+            catch (Exception ex)
+            {
+                Label label = new Label();
+                label.Location = new Point(x + 100, y + 5);
+                label.Size = new Size(600, 400);
+                label.Text = "Probleem: " + ex.ToString();
+                this.Controls.Add((Control)label);
+            }
+        }
+
+        private void ShowWeatherForecast_7days_simple(string location)
+        {
+            this.ClearDynamicInsertedControls();
+            int x = 20;
+            int y = 80;
+            int num = 60;
+            this.Text = "Weersvoorspelling 7 dagen (eenvoudig)";
+            try
+            {
+                HttpResponseMessage result1 = this._httpClient.GetAsync("https://weather.api.here.com/weather/1.0/report.json?app_id=VjXo59SzFVv1SseP8Dxi&app_code=WUutcvuv5eTkLeaVyrd5uQ&language=nl-NL&product=forecast_7days_simple&name=" + location).GetAwaiter().GetResult();
+                string result2 = result1.Content.ReadAsStringAsync().Result;
+                if (result1.StatusCode.ToString() == "OK")
+                {
+                    JArray jarray = (JArray)((JObject)((JObject)JObject.Parse(result2).GetValue("dailyForecasts")).GetValue("forecastLocation")).GetValue("forecast");
+                    this._flp = new FlowLayoutPanel();
+                    this._flp.Location = new Point(x, y);
+                    this._flp.Size = this._flp.Size = this._flpSize;
+                    this._flp.AutoScroll = true;
+                    foreach (JObject jobject in jarray)
+                    {
+                        DateTime dateTime = DateTime.Parse(jobject.GetValue("utcTime").ToString(), (IFormatProvider)null);
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Name = "mypicturebox";
+                        pictureBox.Load(jobject.GetValue("iconLink").ToString());
+                        pictureBox.Location = new Point(x, y);
+                        this._flp.Controls.Add((Control)pictureBox);
+                        Label label = new Label();
+                        label.Name = "mylabel";
+                        label.Location = new Point(x + 100, y + 5);
+                        label.Size = new Size(600, 60);
+                        label.Text = jobject.GetValue("weekday").ToString() + " (" + dateTime.ToString("dd-MMM-yyyy") + ")" + Environment.NewLine + "    " + jobject.GetValue("description")?.ToString() + " " + jobject.GetValue("beaufortDescription")?.ToString() + "   [ " + jobject.GetValue("lowTemperature")?.ToString() + "°C - " + jobject.GetValue("highTemperature")?.ToString() + "°C ]" + Environment.NewLine + "    Regenkans: " + jobject.GetValue("precipitationProbability")?.ToString() + "% - GevoelsTemp: " + jobject.GetValue("comfort").ToString() + " °C";
+                        this._flp.Controls.Add((Control)label);
+                        y += num;
+                    }
+                    this.Controls.Add((Control)this._flp);
+                }
+                else
+                {
+                    Label label = new Label();
+                    label.Location = new Point(x + 100, y + 5);
+                    label.Size = new Size(600, 400);
+                    label.Text = "Probleem: " + result1.StatusCode.ToString() + Environment.NewLine + result2;
+                    this.Controls.Add((Control)label);
+                }
+            }
+            catch (Exception ex)
+            {
+                Label label = new Label();
+                label.Location = new Point(x + 100, y + 5);
+                label.Size = new Size(600, 400);
+                label.Text = "Probleem: " + ex.ToString();
+                this.Controls.Add((Control)label);
+            }
+        }
+
+        // ///////////////
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            btnHechtel_Click(sender, e);
+        }
+
+        // ////////////////
+
+        private void ResetColorAllButtons()
+        {
+            foreach (var button in this.Controls.OfType<Button>())
+            {
+                button.ForeColor = Color.DarkGray;
+            }
+        }
+
+        private void btnHechtel_Click(object sender, EventArgs e)
+        {
+            this.ShowWeatherForecast_7days("Hechtel");
+            ResetColorAllButtons();
+            this.btnHechtel.ForeColor = Color.Blue;
+        }
+      
+        private void btnAntwerpen_Click(object sender, EventArgs e)
+        {
+            this.ShowWeatherForecast_7days("Antwerpen");
+            ResetColorAllButtons();
+            this.btnAntwerpen.ForeColor = Color.Blue;
+        }
+
+        private void btnOostende_Click(object sender, EventArgs e)
+        {
+            this.ShowWeatherForecast_7days("Oostende");
+            ResetColorAllButtons();
+            this.btnOostende.ForeColor = Color.Blue;
+        }
+
+    }
+}
